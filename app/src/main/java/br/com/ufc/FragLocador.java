feec5f5c928b03d80646a71d9a7b2b1aa7dc6e97
MaryzangelaBessa <adm.maryzangelabessa@gmail.com>
2019-10-31 10:57:25 -0300

    Criação dos fragmentos da tela Principal

---
+++ app/src/main/java/br/com/ufc/FragLocador.java
@@ -0,0 +1,22 @@
+package br.com.ufc;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragLocador extends Fragment {
+
+    public FragLocador() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_locador_layout, container, false);
+    }
+
+}
