7a87f39868543c0c8032c448933530e4896a3580
MaryzangelaBessa <adm.maryzangelabessa@gmail.com>
2019-12-05 15:30:32 -0300

    Continuação da listagem de imoveis para locatario

--- app/src/main/java/br/com/ufc/FragLocatario.java
+++ app/src/main/java/br/com/ufc/FragLocatario.java
@@ -2,19 +2,29 @@ package br.com.ufc;
 
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,68 +66,90 @@ public class FragLocatario extends Fragment {
     public void getImoveis(){
 
 
-        CollectionReference imoveis = db.collection("imovel");
-
+        final CollectionReference imoveis = db.collection("imovel");
+//
+//        Query query = imoveis.whereEqualTo("id", true);
+//
+//        List<QuerySnapshot> querySnapshot = (List<QuerySnapshot>) query.get();
+//
+//        for (DocumentSnapshot document : querySnapshot.get(0).getDocuments()) {
+//            Log.d(String.valueOf(getActivity()), "Id do docuemnto: >>> " + document.getId());
+//
+//            System.out.println(document.getId());
+//        }
 
 
 //        List<Imovel> imoveis = (List<Imovel>) db.collection("imovel");
-
+//
 //        for (Imovel imovel: imoveis) {
 //            listaImoveis.add(imovel);
 //        }
 
-//        Imovel imovel = new Imovel("Rua do Sossego", 500f, "Casa");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Rua Oscar Barbosa", 600f, "Casa");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
-//
-//        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
-//        listaImoveis.add(imovel);
+        db.collection("imovel").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                        String end = documentSnapshot.get("nomePro").toString();
+
+                    }
+                }
+            }
+        });
+
+
+
+
+        Imovel imovel = new Imovel("Rua do Sossego", 500f, "Casa");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Rua Oscar Barbosa", 600f, "Casa");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 3", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 4", 123f, "Apartamento");
+        listaImoveis.add(imovel);
+
+        imovel = new Imovel("Endereço 5", 123f, "Apartamento");
+        listaImoveis.add(imovel);
     }
-
-
 
 }
