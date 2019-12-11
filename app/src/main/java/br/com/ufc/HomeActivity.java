60e0dd7fadb3e7eb553f7e1145984e04b0a56e26
MaryzangelaBessa <adm.maryzangelabessa@gmail.com>
2019-12-10 22:34:20 -0300

    Options menu, falta funcionar

--- app/src/main/java/br/com/ufc/HomeActivity.java
+++ app/src/main/java/br/com/ufc/HomeActivity.java
@@ -2,14 +2,18 @@ package br.com.ufc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +36,34 @@ public class HomeActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         textEmail = findViewById(R.id.editEmail);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_editProfile:
+                Toast.makeText(this, "Editar Perfil de User", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_2:
+                Toast.makeText(this, "Ação 2", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_3:
+                Toast.makeText(this, "Ação 3", Toast.LENGTH_SHORT).show();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onCLickCreateCadastro(View view){
